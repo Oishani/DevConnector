69b0305623a8a0b68cf0dbd10022443ead367bea
Oishani Ganguly <titli@Oishanis-MacBook-Air.local>
2020-06-20 04:50:08 +0530

    Custom auth middleware and JWT verify

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 const config = require('config');
 const User = require('../../models/User');
 
